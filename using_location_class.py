7af797158fa9d385d08f4e1185f9450a84a1d1b9
Mike Vosskuhler <mikevosskuhler@gmail.com>
2019-11-09 19:17:31 +0100

    Delete using_location_class.py

--- using_location_class.py
+++
@@ -1,8 +0,0 @@
-#!/home/mike/Documents/python_envs/location_information_inputs/bin/python3
-from location_class import location, currentweather
-from keys import WeatherKey, TomTomKey
-
-LocationAddress = "Amersfoort"
-
-Location = location(LocationAddress, TomTomKey)
-print(Location.Address)
